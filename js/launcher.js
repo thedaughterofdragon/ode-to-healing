b7ed4c9d5d00b7f8d5c3e86fb27a271857482638
thedaughterofdragon <74292218+thedaughterofdragon@users.noreply.github.com>
2021-04-24 20:04:53 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,36 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.bodyA=bodyA
+        this.pointB = pointB
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+    attach(body){
+        this.launcher.bodyA = body;
+    }
+    
+    fly(){
+        this.launcher.bodyA = null;
+    }
+
+    display(){
+       
+        if(this.launcher.bodyA){
+            var pointA = this.bodyA.position;
+            var pointB = this.pointB;
+            
+            
+            strokeWeight(1);
+           line (pointA.x,pointA.y,pointB.x,pointB.y)
+            
+           
+        }
+    }
+    
+}
